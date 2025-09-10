9ed1178c8a7be29c7118f383ce09649a49125142
Starkiller8104 <arielteodocio7@icloud.com>
2025-09-09 20:12:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -852,7 +852,7 @@ if st.button("Generar Excel"):
         ws.insert_image(
         'A1',
         'logo.png',
-        {'x_scale': 0.32, 'y_scale': 0.32}
+        {'x_scale': 0.36, 'y_scale': 0.36}
         )
     except Exception:
         pass
